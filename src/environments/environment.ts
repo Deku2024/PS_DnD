bc60ee9de173cad48e4b2ab63e8a337d2c9c066c
enrique-eg <enrique.esgon@gmail.com>
2026-03-16 16:21:37 +0000

    feat: integrate Firebase into the Angular application
    - Added Firebase SDK dependency to package.json.
    - Created FirebaseService to handle Firebase initialization and services (Auth, Firestore, Storage, Analytics).
    - Added environment configuration files for production and development with Firebase credentials.

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCU82ZPDpZAvQwcrflH96DjfqrovPteQ-8",
+    authDomain: "psdnd-a89df.firebaseapp.com",
+    projectId: "psdnd-a89df",
+    storageBucket: "psdnd-a89df.firebasestorage.app",
+    messagingSenderId: "601004532918",
+    appId: "1:601004532918:web:a8da29fc2041c3af1bb217",
+    measurementId: "G-HESP86VQY0"
+  }
+};
