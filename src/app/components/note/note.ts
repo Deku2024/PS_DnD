124ebdb3d75773138e377f1ad527539ba29ceac4
Carla <cargom2205@gmiail.com>
2026-04-04 19:20:40 +0100

    feat: added createNote function to 'add' button.

--- src/app/components/note/note.ts
+++ src/app/components/note/note.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-note',
-  imports: [],
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './note.html',
   styleUrl: './note.css',
 })
 export class Note {
+  @Input() note!: any;
 
 }
