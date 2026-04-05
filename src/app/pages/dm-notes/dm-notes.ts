bc8c8967c23cd168d3acfcbc9284f5faad28dd2f
Carla <cargom2205@gmiail.com>
2026-04-05 11:44:17 +0100

    styles: done layout for dm-notes page and note styling

--- src/app/pages/dm-notes/dm-notes.ts
+++ src/app/pages/dm-notes/dm-notes.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { Note } from '../../components/note/note';
 
+interface NoteItem {
+  id: number;
+  title: string,
+  content: string;
+}
+
 @Component({
   selector: 'app-dm-notes',
   imports: [Note],
@@ -8,5 +14,31 @@ import { Note } from '../../components/note/note';
   styleUrl: './dm-notes.css',
 })
 export class DmNotes {
+  maxNotes: number = 40;
+  notes: NoteItem[] = [];
+  private nextId = 1;
+
+  createNote() {
+    const newNote: NoteItem = {
+      id: this.nextId++,
+      title: '',
+      content: ''
+    };
+
+    this.notes = [...this.notes, newNote];
+
+    if (this.notes.length > this.maxNotes) {
+        alert("Número de notas excedido.");
+    }
+
+    setTimeout(() => this.scrollToNewNote(), 100);
+  }
+
+  private scrollToNewNote() {
+    const container = document.querySelector('.notes-area');
+    if (container) {
+      container.scrollTop = container.scrollHeight;
+    }
+  }
 
 }
