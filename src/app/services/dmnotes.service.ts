638f78665e36e6616b476e568a9085f26f480207
Carla <cargom2205@gmiail.com>
2026-04-06 16:40:26 +0100

    feat: added service for dmnote persistance.

---
+++ src/app/services/dmnotes.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { FirebaseService } from './firebase.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DmnotesService {
+  constructor(private firebase: FirebaseService) {}
+}
