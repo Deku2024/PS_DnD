f6cf7ed4f549dd021ae05b157e5e59e377b39019
ivanl <ivanlujmor@gmail.com>
2026-04-09 17:39:12 +0100

    fixed: uncomment guardians and route.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,20 +1,20 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, Router, UrlTree } from '@angular/router';
-// import { Observable } from 'rxjs';
-// import { AuthService } from '../services/auth.service';
-//
-// @Injectable({ providedIn: 'root' })
-// export class AuthGuard implements CanActivate {
-//   constructor(private auth: AuthService, private router: Router) {}
-//
-//   canActivate(): Observable<boolean | UrlTree> {
-//     return new Observable(subscriber => {
-//       const sub = this.auth.onAuthState().subscribe(user => {
-//         if (user) subscriber.next(true);
-//         else subscriber.next(this.router.createUrlTree(['/auth']));
-//         subscriber.complete();
-//         sub.unsubscribe();
-//       });
-//     });
-//   }
-// }
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean | UrlTree> {
+    return new Observable(subscriber => {
+      const sub = this.auth.onAuthState().subscribe(user => {
+        if (user) subscriber.next(true);
+        else subscriber.next(this.router.createUrlTree(['/auth']));
+        subscriber.complete();
+        sub.unsubscribe();
+      });
+    });
+  }
+}
