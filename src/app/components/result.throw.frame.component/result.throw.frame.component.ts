eab5c6b211e37864bf610f7fcc28f6ae29c5c2fa
ivanl <ivanlujmor@gmail.com>
2026-04-20 16:15:40 +0100

    pre pull

--- src/app/components/result.throw.frame.component/result.throw.frame.component.ts
+++ src/app/components/result.throw.frame.component/result.throw.frame.component.ts
@@ -10,25 +10,66 @@ import {DiceRollerService, ThrowsResult, TypeOfThrow} from '../../services/roll-
 export class ResultThrowFrameComponent implements OnInit, OnDestroy {
 
   diceRoller = inject(DiceRollerService);
+
   result = signal<ThrowsResult | undefined>(undefined);
+  inAnimation = signal<boolean>(false);
   showResult = signal<boolean>(false);
+  currentNumber = signal<number>(0);
+  delay = signal<number>(10);
 
   private subscription: OutputRefSubscription | null = null;
-  private timer: any = null;
+
+  private showResultTimer: any = null;
+  private animationTimer: any = null;
+  private numberInterval: any = null;
 
   ngOnInit(): void {
     this.subscription = this.diceRoller.lastResult$.subscribe((result) => {
-      this.showResult.set(true);
+
+      this.inAnimation.set(true);
       this.result.set(result);
 
-      if (this.timer !== null) {
-        clearTimeout(this.timer);
-      }
+      this.clearTimers();
+      this.delay.set(10);
 
-      this.timer = setTimeout(() => {
-        this.showResult.set(false);
-      }, 4000);
+      this.changeNumber();
+
+      this.animationTimer = setTimeout(() => {
+
+        this.stopAnimation();
+        this.showResult.set(true);
+
+        this.showResultTimer = setTimeout(() => {
+          this.showResult.set(false);
+        }, 4000);
+
+      }, 5000);
+
     });
+  }
+
+  private stopAnimation() {
+    if (this.numberInterval) {
+      clearTimeout(this.numberInterval);
+      this.numberInterval = null;
+    }
+
+    this.currentNumber.set(<number>this.result()?.result);
+    this.inAnimation.set(false);
+  }
+
+  private clearTimers() {
+    if (this.animationTimer) {
+      clearTimeout(this.animationTimer);
+    }
+
+    if (this.numberInterval) {
+      clearTimeout(this.numberInterval);
+    }
+
+    if (this.showResultTimer) {
+      clearTimeout(this.showResultTimer);
+    }
   }
 
   ngOnDestroy(): void {
@@ -36,8 +77,30 @@ export class ResultThrowFrameComponent implements OnInit, OnDestroy {
   }
 
   interruptDisplay(): void {
-    this.showResult.set(false);
+    if (this.inAnimation()) {
+      this.inAnimation.set(false);
+      this.showResult.set(true);
+    } else {
+      this.showResult.set(false);
+    }
   }
+
+  private changeNumber(): void {
+
+    if (!this.inAnimation()) {
+      return;
+    }
+
+    this.currentNumber.set(Math.floor(Math.random() * this.result()?.side!) + 1);
+    this.delay.set(Math.min(this.delay() * 1.15, 300))
+
+    this.numberInterval.set(setTimeout(() => {
+      this.changeNumber();
+        }, this.delay()
+      )
+    );
+  }
+
 
   protected readonly TypeOfThrow = TypeOfThrow;
 }
