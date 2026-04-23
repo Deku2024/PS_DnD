1791a66190b7531bd6b1096ff88a55d59c01d797
Carla <cargom2205@gmiail.com>
2026-04-23 16:07:47 +0100

    feat: finished monster service

--- src/app/services/monster.service.ts
+++ src/app/services/monster.service.ts
@@ -1,8 +1,60 @@
 import { Injectable } from '@angular/core';
+import { doc, collection, addDoc, deleteDoc, query, onSnapshot, orderBy, updateDoc } from 'firebase/firestore';
+import { FirebaseService } from './firebase.service';
+
+export interface MonsterData {
+  id?: string
+  userId: string;
+  name: string;
+  armourClass: number;
+  race: string;
+  alignment: string;
+  attributes: {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+  };
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class MonsterService {
-  
+  private readonly collection = 'monsters';
+
+  constructor(private Firebase: FirebaseService) {}
+
+  private monsterRef() {
+      return collection(this.Firebase.db, `monster`);
+  }
+
+  async createMonster(monster: MonsterData) {
+    return await addDoc(this.monsterRef(), monster);
+  }
+
+  readMonster(monsterId: string, callback: (monsters: any) => void) {
+    const monsterDoc = doc(this.Firebase.db, `monsters/${monsterId}`);
+    return onSnapshot(monsterDoc, snapshot => {
+      if (snapshot.exists()) {
+        callback({
+          id: snapshot.id,
+          ...snapshot.data()
+        });
+      }
+    });
+  }
+
+  async deleteMonster(monsterId: string) {
+    const monsterDoc = doc(this.Firebase.db, `monsters/${monsterId}`);
+    return await deleteDoc(monsterDoc);
+  }
+
+  async updateMonster(monsterId: string, data: any) {
+    const monsterDoc = doc(this.Firebase.db, `monsters/${monsterId}`);
+
+    return await updateDoc(monsterDoc, data);
+  }  
 }
