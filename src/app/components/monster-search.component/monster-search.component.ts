94e8fc312ec2a565f12e8070fba201d082bb1793
Carla <cargom2205@gmiail.com>
2026-05-03 11:29:59 +0100

    fix: added ChangeDetectorRef in filter component for sync problems,

--- src/app/components/monster-search.component/monster-search.component.ts
+++ src/app/components/monster-search.component/monster-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MonsterService, MonsterData } from '../../services/monster.service';
@@ -22,7 +22,8 @@ export class MonsterSearchComponent implements OnInit {
   sortOrder: 'asc' | 'desc' = 'asc';
 
   constructor(private monsterService: MonsterService,
-              private authService: AuthService) {}
+              private authService: AuthService,
+              private chr: ChangeDetectorRef) {}
 
   ngOnInit() {
     const user = this.authService.getCurrentUser();
@@ -30,6 +31,7 @@ export class MonsterSearchComponent implements OnInit {
       this.monsterService.getMonstersList(user.uid, (monsters) => {
         this.monstersList = monsters;
         this.applyFilters();
+        this.chr.detectChanges();
       });
     }
   }
