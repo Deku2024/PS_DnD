f7863d34193c97edd0fae8cbf7a156ea1cdd4ed9
ivanl <ivanlujmor@gmail.com>
2026-05-11 17:02:53 +0100

    safe commit to switch branchs

--- src/app/services/commerce.service.ts
+++ src/app/services/commerce.service.ts
@@ -8,23 +8,29 @@ import {Merchant} from '../interfaces/Merchant';
 })
 export class CommerceService {
   public sellItemToMerchant(character: CharacterWithId, item: Item, merchant: Merchant) : void {
-    this.removeItemFromPlayerInventory(item, character, merchant);
-    this.addItemToMerchant(item);
+    this.removeItemFromPlayerInventory(item, character, merchant.sellingList[item].price);
+    this.addItemToMerchant(item, merchant);
   }
 
   public buyItemFromMerchant(character: CharacterWithId, item: Item, merchant: Merchant) {
+    this.removeItemFromMerchant(item, merchant);
+    this.addItemToPlayerInventory(item, character, merchant.buyingList[item].price);
   }
 
-  private removeItemFromPlayerInventory(item: Item, character: CharacterWithId, merchant: Merchant) {
+  private removeItemFromPlayerInventory(item: Item, character: CharacterWithId, price: number) {
     // esto se implementará más tarde, cuando se complete la refactorización del inventario
-    character.money + merchant.buyingList[item.id].price;
-  }
-
-  private addItemToMerchant(item: Item) {
-    // esto se implementará más tarde, cuando la definición de mercader esté completa
+    character.money + price;
   }
 
   private addItemToPlayerInventory(item: Item, character: CharacterWithId, price: number) {
-    
+
+  }
+
+  private addItemToMerchant(item: Item, merchant: Merchant) {
+    // esto se implementará más tarde, cuando la definición de mercader esté completa
+  }
+
+  private removeItemFromMerchant(item: Item, merchant: Merchant) {
+
   }
 }
