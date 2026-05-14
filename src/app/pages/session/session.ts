f5e37a0fd0be763b235a41cdec764649ab0365fb
miguelalonsocorimanyarojas-boop <miguelalonsocorimanyarojas@gmail.com>
2026-05-14 23:48:44 +0100

    Fix: fixing parameters in  modal player

--- src/app/pages/session/session.ts
+++ src/app/pages/session/session.ts
@@ -37,6 +37,7 @@ export class SessionPage implements OnInit, OnDestroy {
   characters: { [uid: string]: CharacterWithId | null } = {};
   showModal = false;
   modalCharacter: CharacterWithId | null = null;
+  modalPlayerName = '';
   modalPlayerEmail = '';
   modalUid = '';
   presenceMap: { [uid: string]: boolean } = {};
@@ -155,8 +156,7 @@ export class SessionPage implements OnInit, OnDestroy {
     if (!this.session) return;
     this.modalUid = uid;
     this.modalCharacter = this.characters[uid] ?? null;
-    this.modalPlayerEmail = this.session.playerEmails[uid] || uid;
-    this.showModal = true;
+    this.modalPlayerName = this.session.playersUsernames[uid] || this.session.playerEmails[uid] || 'Jugador';    this.showModal = true;
   }
 
   closeModal(): void {
