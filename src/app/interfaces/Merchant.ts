6242adb9386ff11d757bc7a91f09604e456c6268
Carla <cargom2205@gmiail.com>
2026-05-15 17:30:07 +0100

    feat: interfaces modified

--- src/app/interfaces/Merchant.ts
+++ src/app/interfaces/Merchant.ts
@@ -1,8 +1,8 @@
-import {Item} from './Item';
+import {Item, ItemInfo} from './Item';
 
 export interface Merchant {
   id?: string;
   name: string;
-  sellingList: Record<string, {price: number, quantity: number}>; // lista de venta del mercader -> lo que compran los jugadores
-  buyingList: Record<string, {price: number, quantity: number}>; // lista de compra del mercader -> lo que venden los jugadores
+  sellingList: Record<string, ItemInfo>; //lista de objetos que vende el mercader string: id del Item, ItemInfo: ver interfaz
+  buyingList: Record<string, ItemInfo>; //lista de objetos que compra el mercader string: id del Item, ItemInfo: ver interfaz
 }
